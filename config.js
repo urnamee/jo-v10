f4d321e47b6655c5a491d22aa794572047a4ff17
urnamee <100465709+urnamee@users.noreply.github.com>
2022-12-06 23:29:42 +0700

    Update config.js

--- config.js
+++ config.js
@@ -22,8 +22,8 @@ global.autorecording = false //status auto merekam ( auto record )
 global.autoketik = false //status auto mengetik (auto typing)
 global.available = false //status online (online)
 // Other
-global.owner = ['60177426853','60177426853','60177426853']
-global.premium = ['60177426853']
+global.owner = ['37259323234','37259323234','37259323234']
+global.premium = ['37259323234']
 global.packname = 'The Jo Bot'
 global.author = 'WhatsApp Bot'
 global.wlcm = []
